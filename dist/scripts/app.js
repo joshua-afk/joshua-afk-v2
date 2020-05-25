adabbacbf22dd73acc05f224c78cca2709ff616e
Joshua Ryan Velasquez <joshua.afk.1231@gmail.com>
2020-05-26 04:22:16 +0800

    feat: add show and hide button for tools

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,3 +1,23 @@
-AOS.init({
-    once: true
+// Custom Scripts
+document.addEventListener("DOMContentLoaded", function() {
+    // AOS
+    AOS.init({
+        once: true
+    });
+});
+
+document.getElementById('show_more_tools').addEventListener('click', function(event) {
+    event.preventDefault()
+
+    document.getElementById('show_more_tools_container').style.display = "none";
+    document.getElementById('hide_more_tools_container').style.display = "flex";
+    document.getElementById('more_tools').style.display = "flex";
+});
+
+document.getElementById('hide_more_tools').addEventListener('click', function(event) {
+    event.preventDefault()
+
+    document.getElementById('show_more_tools_container').style.display = "flex";
+    document.getElementById('hide_more_tools_container').style.display = "none";
+    document.getElementById('more_tools').style.display = "none";
 });
