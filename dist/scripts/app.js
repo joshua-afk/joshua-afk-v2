811c5f1a53fe08061bc7b98f389a2ba5708aac7b
Joshua Ryan Velasquez <joshua.afk.1231@gmail.com>
2020-05-27 03:05:33 +0800

    feat: add transition for tools icons container

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -11,13 +11,13 @@ document.getElementById('show_more_tools').addEventListener('click', function(ev
 
     document.getElementById('show_more_tools_container').style.display = "none";
     document.getElementById('hide_more_tools_container').style.display = "flex";
-    document.getElementById('more_tools').style.display = "flex";
+    document.getElementById('more_tools').style.height = "1105px";
 });
 
 document.getElementById('hide_more_tools').addEventListener('click', function(event) {
     event.preventDefault()
 
-    document.getElementById('show_more_tools_container').style.display = "flex";
     document.getElementById('hide_more_tools_container').style.display = "none";
-    document.getElementById('more_tools').style.display = "none";
+    document.getElementById('show_more_tools_container').style.display = "flex";
+    document.getElementById('more_tools').style.height = "75px";
 });
