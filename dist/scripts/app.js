8be97fe6c1f1bb7b2e080861820c41725430bc06
Joshua Ryan Velasquez <joshua.afk.1231@gmail.com>
2020-05-28 05:53:57 +0800

    feat: add navigation bar with transition effect

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+// Tools
 document.getElementById('show_more_tools').addEventListener('click', function(event) {
     event.preventDefault()
 
@@ -20,4 +21,43 @@ document.getElementById('hide_more_tools').addEventListener('click', function(ev
     document.getElementById('hide_more_tools_container').style.display = "none";
     document.getElementById('show_more_tools_container').style.display = "flex";
     document.getElementById('more_tools').style.height = "75px";
+});
+
+
+// Nav Menu
+var nav            = document.getElementById('nav'); 
+var nav_menu       = document.getElementById('nav_menu'); 
+var nav_menu_open  = document.getElementById('nav_menu_open'); 
+var nav_menu_close = document.getElementById('nav_menu_close'); 
+
+nav_menu_open.addEventListener('mouseover', function(event) {
+    nav_menu.style.clipPath = "circle(20% at 100% 0%)";
+});
+
+nav_menu_open.addEventListener('mouseout', function(event) {
+    nav_menu.style.clipPath = "circle(0% at 100% 0%)";
+});
+
+nav_menu_open.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    nav_menu_open.style.display = "none";
+    nav_menu_close.style.display = "block";
+
+    nav.style.position = "fixed";
+    nav_menu.style.clipPath = "circle(100%)";
+});
+
+nav_menu_close.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    nav_menu_open.style.display = "block";
+    nav_menu_close.style.display = "none";
+
+    nav.style.position = "absolute";
+    nav_menustyle.clipPath = "circle(0% at 100% 0%)";
+});
+
+nav_menu_close.addEventListener('mouseover', function(event) {
+    nav_menu.style.clipPath = "circle(100%)";
 });
