c735cfc72bc734efa82e0204e86e8eec733b5f6b
Joshua Ryan Velasquez <joshua.afk.1231@gmail.com>
2020-06-11 18:10:05 +0800

    refactor: fix master js code format

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,34 +1,33 @@
 // Variables
-var nav            = document.getElementById('nav'); 
-var nav_menu       = document.getElementById('nav_menu'); 
-var nav_icon_white = document.getElementById('nav_icon_white'); 
-var nav_icon_black = document.getElementById('nav_icon_black'); 
-var nav_menu_open  = document.getElementById('nav_menu_open'); 
-var nav_menu_close = document.getElementById('nav_menu_close'); 
+var nav = document.getElementById('nav');
+var nav_menu = document.getElementById('nav_menu');
+var nav_icon_white = document.getElementById('nav_icon_white');
+var nav_icon_black = document.getElementById('nav_icon_black');
+var nav_menu_open = document.getElementById('nav_menu_open');
+var nav_menu_close = document.getElementById('nav_menu_close');
 
 // Custom Scripts
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // AOS
     AOS.init({
         once: true
     });
-
 });
 
-window.addEventListener("scroll", function(event) {
-    if (this.scrollY < 100) {
-        nav.classList.add("nav-transparent")
-        nav.classList.remove("nav-white")
-        console.log('less than 100vh');
-    } else {
-        nav.classList.add("nav-white")
-        nav.classList.remove("nav-transparent")
-        console.log('greater than 100vh');
-    }
-});
+// window.addEventListener("scroll", function () {
+//     if (this.scrollY < 100) {
+//         nav.classList.add("nav-transparent")
+//         nav.classList.remove("nav-white")
+//         console.log('less than 100vh');
+//     } else {
+//         nav.classList.add("nav-white")
+//         nav.classList.remove("nav-transparent")
+//         console.log('greater than 100vh');
+//     }
+// });
 
 // Tools
-document.getElementById('show_more_tools').addEventListener('click', function(event) {
+document.getElementById('show_more_tools').addEventListener('click', function (event) {
     event.preventDefault()
 
     document.getElementById('show_more_tools_container').style.display = "none";
@@ -36,7 +35,7 @@ document.getElementById('show_more_tools').addEventListener('click', function(ev
     document.getElementById('more_tools').style.height = "1105px";
 });
 
-document.getElementById('hide_more_tools').addEventListener('click', function(event) {
+document.getElementById('hide_more_tools').addEventListener('click', function (event) {
     event.preventDefault()
 
     document.getElementById('hide_more_tools_container').style.display = "none";
@@ -44,22 +43,21 @@ document.getElementById('hide_more_tools').addEventListener('click', function(ev
     document.getElementById('more_tools').style.height = "75px";
 });
 
-
-nav_menu_open.addEventListener('mouseover', function(event) {
-    if(window.innerWidth < 1200){
+nav_menu_open.addEventListener('mouseover', function () {
+    if (window.innerWidth < 1200) {
         nav_menu.style.clipPath = "circle(25% at 100% 4%)";
-    } else if(window.innerWidth <= 1522){
+    } else if (window.innerWidth <= 1522) {
         nav_menu.style.clipPath = "circle(30% at 100% -10%)";
     } else {
         nav_menu.style.clipPath = "circle(50% at 100% -30%)";
     }
 });
 
-nav_menu_open.addEventListener('mouseout', function(event) {
+nav_menu_open.addEventListener('mouseout', function () {
     nav_menu.style.clipPath = "circle(0% at 100% 0%)";
 });
 
-nav_menu_open.addEventListener('click', function(event) {
+nav_menu_open.addEventListener('click', function (event) {
     event.preventDefault()
 
     nav_menu_open.style.display = "none";
@@ -72,7 +70,7 @@ nav_menu_open.addEventListener('click', function(event) {
     nav_menu.style.clipPath = "circle(100%)";
 });
 
-nav_menu_close.addEventListener('click', function(event) {
+nav_menu_close.addEventListener('click', function (event) {
     event.preventDefault()
 
     nav_menu_open.style.display = "block";
@@ -85,7 +83,7 @@ nav_menu_close.addEventListener('click', function(event) {
     nav_menu.style.clipPath = "circle(0% at 100% 0%)";
 });
 
-nav_menu_close.addEventListener('mouseover', function(event) {
+nav_menu_close.addEventListener('mouseover', function () {
     nav_menu.style.clipPath = "circle(100%)";
 });
 
