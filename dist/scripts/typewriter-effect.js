2a3d26b0ca298a0adea87efa3142b5a4ee9afa67
Joshua Ryan Velasquez <joshua.afk.1231@gmail.com>
2020-06-16 22:56:28 +0800

    fix: better typewritter effect timing

--- dist/scripts/typewriter-effect.js
+++ dist/scripts/typewriter-effect.js
@@ -1,55 +1,55 @@
 // ES6 Class
 class TypeWriter {
-    constructor(txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
+    constructor(txtElement, words, wait = 4000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 40);
+        this.type();
+        this.isDeleting = false;
     }
 
     type() {
-    // Current index of data-word
-    const current = this.wordIndex % this.words.length;
-    // Get full text of current data-word
-    const fullTxt = this.words[current];
+        // Current index of data-word
+        const current = this.wordIndex % this.words.length;
+        // Get full text of current data-word
+        const fullTxt = this.words[current];
 
-    // Check if deleting
-    if(this.isDeleting) {
-        // Remove char
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        // Add char
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+        // Check if deleting
+        if (this.isDeleting) {
+            // Remove char
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            // Add char
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
 
-    // Insert txt into element (span)
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+        // Insert txt into element (span)
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    // Init Type Speed
-    let typeSpeed = 200;
+        // Init Type Speed
+        let typeSpeed = 150;
 
-    if(this.isDeleting) {
-        typeSpeed /= 2;
-    }
+        if (this.isDeleting) {
+            typeSpeed /= 2;
+        }
 
-    // If word is complete 
-    if(!this.isDeleting && this.txt === fullTxt) {
-        // Make pause at end of word
-        typeSpeed = this.wait;
-        // Set delete to true
-        this.isDeleting = true;
-    } else if(this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        // Move to the next word
-        this.wordIndex++;
-        // Pause before start typing
-        typeSpeed = 400;
-    }
+        // If word is complete 
+        if (!this.isDeleting && this.txt === fullTxt) {
+            // Make pause at end of word
+            typeSpeed = this.wait;
+            // Set delete to true
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            // Move to the next word
+            this.wordIndex++;
+            // Pause before start typing
+            typeSpeed = 500;
+        }
 
-    setTimeout(() => this.type(), typeSpeed);
+        setTimeout(() => this.type(), typeSpeed);
     }
 };
 
